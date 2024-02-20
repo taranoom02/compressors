c4b079eeb518d828152801b3b77bb98de8280aca
Sana Taranoom <sanargukt.555@gmail.com>
2024-02-20 20:49:59 +0530

    live

---
+++ compressor/docs/server/chunk-MCAGWG5H.mjs
@@ -0,0 +1,88 @@
+import './polyfills.server.mjs';
+import{C as S,F as x,a as m,b as l,c as d,d as C,e as f,f as b,g as h,h as n,i,j as a,k as o,l as v,t as y,u as w,v as M,y as O}from"./chunk-D7EYIDSY.mjs";var P=(()=>{let e=class e{};e.\u0275fac=function(r){return new(r||e)},e.\u0275cmp=m({type:e,selectors:[["app-about"]],decls:37,vars:0,consts:[["lang","en"],["charset","UTF-8"],["name","viewport","content","width=device-width, initial-scale=1.0"],["href","https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/css/bootstrap.min.css","rel","stylesheet"],[1,"container","mt-5"],[1,"text-center","mb-4"],[1,"text-center"],[1,"row","mt-5"],[1,"col-md-8","mx-auto"],[1,"mb-3"],["href","mailto:sanargukt.555@gmail.com"]],template:function(r,p){r&1&&(n(0,"html",0)(1,"head"),a(2,"meta",1)(3,"meta",2),n(4,"title"),o(5,"About Us"),i(),a(6,"link",3),i(),n(7,"body")(8,"div",4)(9,"h2",5),o(10,"Welcome to Umeza Generators and Compressors"),i(),n(11,"p",6),o(12,"Your trusted source for reliable used compressors and generators."),i(),n(13,"div",7)(14,"div",8)(15,"h3",9),o(16,"We Sell Used Compressors and Generators"),i(),n(17,"p"),o(18,"At Umeza Generators and Compressors, we specialize in providing top-quality used compressors and generators at affordable prices. Our products undergo rigorous testing to ensure they are in good running condition, so you can trust that you're getting reliable equipment for your needs."),i(),n(19,"p"),o(20,"Whether you're looking for compressors or generators for industrial, commercial, or residential use, we have a wide range of options available to suit your requirements. From small-scale projects to large-scale operations, we've got you covered."),i(),n(21,"p"),o(22,"What sets us apart is our commitment to customer satisfaction. We strive to provide exceptional service and support to our clients every step of the way. Our knowledgeable team is here to assist you in finding the perfect solution for your power needs."),i(),n(23,"p"),o(24,"Don't hesitate to reach out to us if you have any questions or if you're interested in making a purchase. Contact us at "),n(25,"strong"),o(26,"9949030770"),i(),o(27," or email us at "),n(28,"a",10),o(29,"sanargukt.555@gmail.com"),i(),o(30,". We look forward to serving you!"),i(),n(31,"p"),o(32,"Best Regards,"),a(33,"br"),o(34,"Shaik Chand"),a(35,"br"),o(36,"Umeza Generators and Compressors"),i()()()()()())},styles:["body[_ngcontent-%COMP%]{font-family:Arial,sans-serif;background-color:#f8f9fa}.container[_ngcontent-%COMP%]{max-width:800px;margin:0 auto;padding:20px;background-color:#fff;border-radius:10px;box-shadow:0 0 10px #0000001a}h2[_ngcontent-%COMP%]{color:#333}h3[_ngcontent-%COMP%]{color:#007bff}p[_ngcontent-%COMP%]{color:#666;line-height:1.6}a[_ngcontent-%COMP%]{color:#007bff;text-decoration:none}a[_ngcontent-%COMP%]:hover{text-decoration:underline}"]});let t=e;return t})();var I=[{path:"about",component:P}],k=(()=>{let e=class e{};e.\u0275fac=function(r){return new(r||e)},e.\u0275mod=l({type:e}),e.\u0275inj=d({imports:[x.forRoot(I),x]});let t=e;return t})();var E=(()=>{let e=class e{};e.\u0275fac=function(r){return new(r||e)},e.\u0275cmp=m({type:e,selectors:[["app-navbar"]],decls:12,vars:0,consts:[["lang","en"],["charset","UTF-8"],["name","viewport","content","width=device-width, initial-scale=1.0"],[1,"navbar"],[1,"navbar-brand"],["href","/about",1,"navbar-link"]],template:function(r,p){r&1&&(n(0,"html",0)(1,"head"),a(2,"meta",1)(3,"meta",2),n(4,"title"),o(5,"Navbar with Image"),i()(),n(6,"body")(7,"nav",3)(8,"span",4),o(9,"Umeza Generators and Compressors"),i(),n(10,"a",5),o(11,"About Us"),i()()()())},styles:[`
+
+    .navbar[_ngcontent-%COMP%] {
+      background-color: #ff7b00;
+      padding: 10px; 
+
+      display: flex;
+      justify-content: space-between; 
+
+      align-items: center; 
+
+    }
+
+    
+
+    .navbar-brand[_ngcontent-%COMP%] {
+      color: #ffffff; 
+
+      font-weight: bold; 
+
+      font-size: 1.5rem; 
+
+    }
+
+    
+
+    .navbar-link[_ngcontent-%COMP%] {
+      color: #ffffff; 
+
+      text-decoration: none; 
+
+      font-weight: bold; 
+
+    }
+
+    
+
+    .navbar-link[_ngcontent-%COMP%]:hover {
+      color: #d9d9d9; 
+
+    }`]});let t=e;return t})();function D(t,e){if(t&1&&(n(0,"div",9)(1,"div",10),a(2,"img",11),n(3,"div",12)(4,"h5",13),o(5),i(),n(6,"p",14),o(7),i()()()()),t&2){let g=e.$implicit;f(2),h("src",g.imageUrl,C)("alt",g.imageAlt),f(3),v(g.title),f(2),v(g.description)}}var F=(()=>{let e=class e{constructor(){this.cards=[{imageUrl:"assets/images/download.jpeg",imageAlt:"Card 1 Image",title:"Compressor",description:"working Condition"},{imageUrl:"assets/images/1.jpeg",imageAlt:"Card 2 Image",title:"Generator",description:"Good Condition"},{imageUrl:"assets/images/3.jpeg",imageAlt:"Card 3 Image",title:"Generator",description:"In Working"},{imageUrl:"assets/images/6.jpeg",imageAlt:"Card 1 Image",title:"Compressor",description:"working in good condition"},{imageUrl:"assets/images/7.jpeg",imageAlt:"Card 2 Image",title:"Compressor",description:"working in good condition"},{imageUrl:"assets/images/8.jpeg",imageAlt:"Card 3 Image",title:"Generator",description:"In Working"}]}};e.\u0275fac=function(r){return new(r||e)},e.\u0275cmp=m({type:e,selectors:[["app-main"]],decls:17,vars:1,consts:[["lang","en"],["charset","UTF-8"],["name","viewport","content","width=device-width, initial-scale=1.0"],["href","https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/css/bootstrap.min.css","rel","stylesheet"],[1,"h"],[1,"container"],[1,"row"],["class","col-md-4",4,"ngFor","ngForOf"],[1,"imp"],[1,"col-md-4"],[1,"card"],[1,"card-img-top",3,"src","alt"],[1,"card-body"],[1,"card-title"],[1,"card-text"]],template:function(r,p){r&1&&(n(0,"html",0)(1,"head"),a(2,"meta",1)(3,"meta",2),n(4,"title"),o(5,"Three Cards in One Row"),i(),a(6,"link",3),i(),n(7,"body")(8,"div",4)(9,"h1"),o(10,"Our Products"),i()(),n(11,"div",5)(12,"div",6),b(13,D,8,4,"div",7),i()(),n(14,"div",8)(15,"h1"),o(16,"For more Details Contact at 9949030770"),i()()()()),r&2&&(f(13),h("ngForOf",p.cards))},dependencies:[y],styles:[".card[_ngcontent-%COMP%]{border-radius:15px;box-shadow:0 0 10px #0000001a}.card-img-top[_ngcontent-%COMP%]{border-top-left-radius:15px;border-top-right-radius:15px}.card-body[_ngcontent-%COMP%]{padding:20px}.h[_ngcontent-%COMP%]{text-align:center}.mb-3[_ngcontent-%COMP%]{margin-bottom:5rem}.imp[_ngcontent-%COMP%]{text-align:center}",`.card[_ngcontent-%COMP%] {
+      width: 250px; 
+
+      height: 400px; 
+
+    }
+    .card-img-top[_ngcontent-%COMP%] {
+      height: 50%; 
+
+    }
+    .card-body[_ngcontent-%COMP%] {
+      height: 50%; 
+
+      overflow: hidden; 
+
+    }
+    .h[_ngcontent-%COMP%] {
+      text-align: center; 
+
+    }`]});let t=e;return t})();var U=(()=>{let e=class e{};e.\u0275fac=function(r){return new(r||e)},e.\u0275cmp=m({type:e,selectors:[["app-footer"]],decls:12,vars:0,consts:[["lang","en"],["charset","UTF-8"],["name","viewport","content","width=device-width, initial-scale=1.0"],["href","https://www.youtube.com/@sanataranoom7994"]],template:function(r,p){r&1&&(n(0,"html",0)(1,"head"),a(2,"meta",1)(3,"meta",2),n(4,"title"),o(5,"Footer with Link"),i()(),n(6,"body")(7,"footer")(8,"p"),o(9,"For more details visit our channel "),n(10,"a",3),o(11,"https://www.youtube.com/@sanataranoom7994"),i()()()()())},styles:[`
+
+    footer[_ngcontent-%COMP%] {
+      background-color: #333; 
+
+      color: white; 
+
+      padding: 20px; 
+
+      text-align: center; 
+
+    }
+
+    
+
+    footer[_ngcontent-%COMP%]   a[_ngcontent-%COMP%] {
+      color: white; 
+
+      text-decoration: none; 
+
+    }
+
+    
+
+    footer[_ngcontent-%COMP%]   a[_ngcontent-%COMP%]:hover {
+      text-decoration: underline; 
+
+    }`]});let t=e;return t})();var u=(()=>{let e=class e{constructor(){this.title="compressor"}};e.\u0275fac=function(r){return new(r||e)},e.\u0275cmp=m({type:e,selectors:[["app-root"]],decls:4,vars:0,template:function(r,p){r&1&&a(0,"app-navbar")(1,"router-outlet")(2,"app-main")(3,"app-footer")},dependencies:[S,E,F,U]});let t=e;return t})();var j=(()=>{let e=class e{};e.\u0275fac=function(r){return new(r||e)},e.\u0275mod=l({type:e,bootstrap:[u]}),e.\u0275inj=d({providers:[M()],imports:[w,k]});let t=e;return t})();var G=(()=>{let e=class e{};e.\u0275fac=function(r){return new(r||e)},e.\u0275mod=l({type:e,bootstrap:[u]}),e.\u0275inj=d({imports:[j,O]});let t=e;return t})();export{G as a};
